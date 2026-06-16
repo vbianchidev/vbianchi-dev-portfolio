1417352098fbfacd90101353e9841578bf4942ff
vbianchi.dev <vitor.biansil@gmail.com>
2026-06-16 18:05:29 -0300

    sync

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,7 +12,7 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideTranslateService({
       loader: provideTranslateHttpLoader({
-        prefix: '/assets/i18n/',
+        prefix: 'assets/i18n/',
         suffix: '.json',
       }),
       fallbackLang: 'pt-br',
