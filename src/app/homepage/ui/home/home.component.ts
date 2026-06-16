8ff09201327a8c0d53ddb01b7bf9a301dc71c2ea
vbianchi.dev <vitor.biansil@gmail.com>
2026-06-16 16:08:24 -0300

    sync

--- src/app/homepage/ui/home/home.component.ts
+++ src/app/homepage/ui/home/home.component.ts
@@ -23,13 +23,7 @@ export class HomeComponent {
       type: 'github',
       name: 'VBianchi Dev Portfolio',
       desc: 'Projeto de portfólio pessoal hospedado no GitHub, apresentando minhas habilidades, projetos e experiências profissionais.',
-      url: 'https://github.com/seu-usuario/projeto-1',
-    },
-    {
-      type: 'figma',
-      name: 'VBianchi Dev Portfolio',
-      desc: 'Protótipo ou design system criado para um produto ou cliente.',
-      url: 'https://www.figma.com/file/seu-link',
+      url: 'https://github.com/vbianchidev/vbianchi-dev-portfolio',
     },
   ];
 
